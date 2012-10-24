e183654e70ec5ce58edc4e26a14b311c70a22f3b
Jing Zhang <zhjxjtu@gmail.com>
2012-10-24 10:30:54 -0700

    Master commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Focus::Application.config.secret_token = 'db3bef9c578950d128455b5b2cce88955634fc9ce5e74370ab2a6a871436f589db04098f5737f1b0ce3dd9dca360a396b095ef751c2c85570c3a681e892cab7d'
