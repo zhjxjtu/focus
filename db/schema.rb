7bdb859a226ae9a588315b972000cba549d97a4f
Jing Zhang <zhjxjtu@gmail.com>
2012-11-13 06:36:02 -0800

    Rollback from 1st try of relationship model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121109034442) do
+ActiveRecord::Schema.define(:version => 20121110170142) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
